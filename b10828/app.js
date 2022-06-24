405bb8658a991ceced707e98a62886851be00148
[SW2/서울/2반]성경주 <yesican1011@naver.com>
2022-06-24 20:47:13 +0900

    스택 문제

---
+++ b10828/app.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
+const N = input.shift();
+const stack = [];
+let output = "";
+
+for (i = 0; i < N; i++) {
+  let command = input[i].split(" ")[0].replace(/\r/g, "");
+  let value = input[i].split(" ")[1];
+  switch (command) {
+    case "push":
+      stack.push(value);
+      break;
+
+    case "pop":
+      if (stack.length === 0) {
+        output += "-1\n";
+        break;
+      }
+      output += stack.pop() + "\n";
+      break;
+
+    case "size":
+      output += stack.length + "\n";
+      break;
+
+    case "empty":
+      if (stack.length === 0) {
+        output += "1\n";
+        break;
+      }
+      output += "0\n";
+      break;
+
+    case "top":
+      if (stack.length === 0) {
+        output += "-1\n";
+        break;
+      }
+      output += stack[stack.length - 1] + "\n";
+      break;
+  }
+}
+console.log(output);
