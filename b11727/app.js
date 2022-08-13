ab1484213aa4eaf2064402dccc5122ffbfa2f764
[SW2/서울/2반]성경주 <yesican1011@naver.com>
2022-08-13 15:39:37 +0900

    백준 11726, 11727
    2*n 타일링 1,2
    DP문제

---
+++ b11727/app.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim();
+const arr = [1, 1];
+
+for (let i = 2; i <= input; i++) {
+  arr[i] = (arr[i - 1] + 2 * arr[i - 2]) % 10007;
+}
+console.log(arr[input]);
