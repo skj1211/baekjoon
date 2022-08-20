0afc235c5b9e9e39ef4557f978602da9b531f296
[SW2/서울/2반]성경주 <yesican1011@naver.com>
2022-08-20 16:32:17 +0900

    백준 1260 DFS,BFS
    DFS와 BFS 이해를 위해 다시 풀어본것

--- b1260/app.js
+++ b1260/app.js
@@ -1,28 +1,26 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
-let [n, m, v] = input[0].split(" ").map(Number);
-let graph = new Array(n + 1);
-for (let i = 0; i < graph.length; i++) {
-  graph[i] = [];
-}
+const [n, m, v] = input.shift().split(" ").map(Number);
+const graph = [...Array(n + 1)].map((el) => []);
+
 for (let i = 0; i < m; i++) {
-  let [from, to] = input[i + 1].split(" ").map(Number);
+  const [from, to] = input[i].split(" ").map(Number);
   graph[from].push(to);
   graph[to].push(from);
 }
 graph.forEach((element) => {
   element.sort((a, b) => a - b);
 });
+
 let visited = new Array(n + 1).fill(0);
 let answer_dfs = [];
-// DFS
 function DFS(v) {
-  if (visited[v]) return;
+  if (visited[v]) retrun;
   visited[v] = 1;
   answer_dfs.push(v);
   for (let i = 0; i < graph[v].length; i++) {
-    let next = graph[v][i];
+    const next = graph[v][i];
     if (visited[next] === 0) {
       DFS(next);
     }
@@ -30,9 +28,9 @@ function DFS(v) {
 }
 DFS(v);
 console.log(answer_dfs.join(" "));
-// BFS
-let answer_bfs = [];
 visited.fill(0);
+
+let answer_bfs = [];
 function BFS(v) {
   let queue = [v];
   while (queue.length) {
@@ -52,5 +50,61 @@ function BFS(v) {
 }
 BFS(v);
 console.log(answer_bfs.join(" "));
+
+//////////////////////////////////////////
+// let [n, m, v] = input[0].split(" ").map(Number);
+// let graph = new Array(n + 1);
+// for (let i = 0; i < graph.length; i++) {
+//   graph[i] = [];
+// }
+// for (let i = 0; i < m; i++) {
+//   let [from, to] = input[i + 1].split(" ").map(Number);
+//   graph[from].push(to);
+//   graph[to].push(from);
+// }
+// graph.forEach((element) => {
+//   element.sort((a, b) => a - b);
+// });
+
+// let visited = new Array(n + 1).fill(0);
+// let answer_dfs = [];
+
+// // DFS
+// function DFS(v) {
+//   if (visited[v]) return;
+//   visited[v] = 1;
+//   answer_dfs.push(v);
+//   for (let i = 0; i < graph[v].length; i++) {
+//     let next = graph[v][i];
+//     if (visited[next] === 0) {
+//       DFS(next);
+//     }
+//   }
+// }
+// DFS(v);
+// console.log(answer_dfs.join(" "));
+
+// // BFS
+// let answer_bfs = [];
+// visited.fill(0);
+// function BFS(v) {
+//   let queue = [v];
+//   while (queue.length) {
+//     let x = queue.shift();
+//     if (visited[x] === 1) {
+//       continue;
+//     }
+//     visited[x] = 1;
+//     answer_bfs.push(x);
+//     for (let i = 0; i < graph[x].length; i++) {
+//       let next = graph[x][i];
+//       if (visited[next] === 0) {
+//         queue.push(next);
+//       }
+//     }
+//   }
+// }
+// BFS(v);
+// console.log(answer_bfs.join(" "));
 
 //https://velog.io/@ywc8851/%EB%B0%B1%EC%A4%80-1260-DFS%EC%99%80-BFS-javascript
