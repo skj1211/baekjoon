a7f269c689c8544e2ca8fe39926747fb1c6be219
[SW2/서울/2반]성경주 <yesican1011@naver.com>
2022-08-26 18:50:21 +0900

    백준 11724 연결요소의개수 BFS
    이전에 DFS로 해결해서 BFS로 다시 해결함

--- b11724/app.js
+++ b11724/app.js
@@ -2,8 +2,43 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 const [n, m] = input.shift().split(" ").map(Number);
-const graph = [];
+
+/////////////////////////////////
+//DFS
+// const graph = [];
+// const visited = new Array(n + 1).fill(0);
+// for (let i = 1; i <= n; i++) {
+//   graph[i] = [];
+// }
+// for (let i = 0; i < m; i++) {
+//   const [from, to] = input[i].split(" ").map(Number);
+//   graph[from].push(to);
+//   graph[to].push(from);
+// }
+
+// function DFS(start) {
+//   visited[start] = 1;
+//   for (let i = 0; i < graph[start].length; i++) {
+//     const next = graph[start][i];
+//     if (visited[next] === 0) {
+//       DFS(next);
+//     }
+//   }
+// }
+
+// let count = 0;
+// for (let i = 1; i <= n; i++) {
+//   if (visited[i] === 0) {
+//     DFS(i);
+//     count++;
+//   }
+// }
+// console.log(count);
+
+/////////////////////////////
+//BFS
 const visited = new Array(n + 1).fill(0);
+const graph = [];
 for (let i = 1; i <= n; i++) {
   graph[i] = [];
 }
@@ -13,21 +48,28 @@ for (let i = 0; i < m; i++) {
   graph[to].push(from);
 }
 
-function DFS(start) {
-  visited[start] = 1;
-  for (let i = 0; i < graph[start].length; i++) {
-    let next = graph[start][i];
-    if (visited[next] === 0) {
-      DFS(next);
+function BFS(start) {
+  const queue = [start];
+
+  while (queue.length) {
+    start = queue.pop();
+    if (visited[start] === 1) {
+      continue;
+    }
+    visited[start] = 1;
+    for (let i = 0; i < graph[start].length; i++) {
+      if (visited[graph[start][i]] === 0) {
+        queue.push(graph[start][i]);
+      }
     }
   }
 }
 
-let output = 0;
+let count = 0;
 for (let i = 1; i <= n; i++) {
   if (visited[i] === 0) {
-    DFS(i);
-    output++;
+    BFS(i);
+    count++;
   }
 }
-console.log(output);
+console.log(count);
