063fbf9007fb713b4d2da2353e5950c2d403654e
[SW2/서울/2반]성경주 <yesican1011@naver.com>
2022-09-07 16:12:09 +0900

    백준 7576 토마토
    BFS

--- b7576/app.js
+++ b7576/app.js
@@ -1,28 +1,50 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
-const [m, n] = input.shift().split(" ").map(Number);
-input = input.map((el) => el.split(" ").map(Number));
-let count = 0;
-
-const moveX = [0, 0, 1, -1];
-const moveY = [1, -1, 0, 0];
+const ds = [
+  [-1, 0],
+  [1, 0],
+  [0, 1],
+  [0, -1],
+];
+const [M, N] = input[0].split(" ").map(Number);
 const queue = [];
-const box = [];
-console.log(input);
-for (let i = 0; i < n; i++) {
-  //   const arr = input[i].split(" ").map((v) => +v);
-  //   box.push(arr); // 각 행의 토마토 정보 배열를 박스 행렬에 담는다.
-  let idx = -1;
-  while (true) {
-    // 박스에서 익은 토마토가 있다면 좌표를 queue에 넣어준다.
-    idx = input.indexOf[i](1, idx + 1);
-    if (idx === -1) break;
-    queue.push([i - 1, idx]);
-  }
-}
-console.log(input);
-console.log(box);
-console.log(queue);
+const visit = [...Array(N)].map((e) => Array(M).fill(0));
+let count = M * N;
+let answer;
 
-function BFS() {}
+for (let i = 1; i < input.length; i++) {
+  let box = input[i].split(" ").map(Number);
+
+  box.forEach((tomato, pos) => {
+    if (tomato === 1) {
+      queue.push([i - 1, pos, 0]);
+      visit[i - 1][pos] = 1;
+      count--;
+    } else if (tomato === -1) {
+      visit[i - 1][pos] = 1;
+      count--;
+    }
+  });
+}
+
+let idx = 0;
+while (queue.length != idx) {
+  const [x, y, pos] = queue[idx];
+  for (let i = 0; i < 4; i++) {
+    const xPos = x + ds[i][0];
+    const yPos = y + ds[i][1];
+
+    if (xPos < 0 || yPos < 0 || xPos >= N || yPos >= M) continue;
+    if (!visit[xPos][yPos]) {
+      visit[xPos][yPos] = 1;
+      queue.push([xPos, yPos, pos + 1]);
+      count--;
+    }
+  }
+
+  idx++;
+  answer = pos;
+}
+
+console.log(count ? -1 : answer);
